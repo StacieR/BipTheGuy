772c8a2ee7b0d924fcbd1e575b2865a75978074d
Stacie Rivera <stacierivera96@gmail.com>
2026-04-09 11:19:58 -0700

    Initial Commit

---
+++ BipTheGuy/BipTheGuyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BipTheGuyApp.swift
+//  BipTheGuy
+//
+//  Created by RIVERA, STACIE on 4/9/26.
+//
+
+import SwiftUI
+
+@main
+struct BipTheGuyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
